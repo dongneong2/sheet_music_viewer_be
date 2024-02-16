da5af48ce562c6120eb19f4b871fbda70630db45
김도영 <dongneong2@naver.com>
2024-02-16 18:20:00 +0900

    first commit

---
+++ src/main/java/com/personal/sheetmusic/SheetmusicApplication.java
@@ -0,0 +1,13 @@
+package com.personal.sheetmusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheetmusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheetmusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/sheetmusic/SheetmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.sheetmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SheetmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
